1699c273181aeb2665b0df9d164d0a2182cfd22b
Rhal Rojas <rhalandrew@gmail.com>
2026-07-23 07:24:26 +0000

    Adding MySQL

--- core/settings.py
+++ core/settings.py
@@ -72,17 +72,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'postgres',
+	# 'USER': 'postgres',
+	# 'PASSWORD': 'postgres',
+	# 'HOST': 'db',
+	# 'PORT': 5432,
+    # }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-	'USER': 'postgres',
-	'PASSWORD': 'postgres',
+	'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'django-app-db',
+	'USER': 'root',
+	'PASSWORD': '',
 	'HOST': 'db',
-	'PORT': 5432,
+	'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
